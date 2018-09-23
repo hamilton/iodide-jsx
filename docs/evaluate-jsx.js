559ffc4105d1b6fba6f9332b50a95eba78789b9c
hamilton <hamilton.ulmer@gmail.com>
2018-09-23 15:43:34 -0700

    added a new iodide.exportJSX for shipping JSX out of a cell

--- docs/evaluate-jsx.js
+++ docs/evaluate-jsx.js
@@ -15,9 +15,14 @@ const loadResource = url => new Promise((resolve) => {
 Promise.all([loadResource(REACT), loadResource(REACT_DOM), loadResource(BABEL_STANDALONE)])
   .then(() => {
     window.jsx = {}
-    window.jsx.transpile = input => window.Babel.transform(input, { presets: ['es2015', 'react'] }).code
+    window.jsx.transpile = input => window.Babel.transform(input, {
+      presets: [['es2015', { loose: true, modules: false }], 'react'],
+    }).code
     window.jsx.evaluateJSX = input => eval(window.jsx.transpile(input))  // eslint-disable-line
-
+    window.iodide.exportJSX = (variableName, variable) => {
+      const out = variable
+      window[variableName] = out;
+    }
     window.iodide.addOutputHandler({
       shouldHandle: val => window.React.isValidElement(val),
       render: (val) => {

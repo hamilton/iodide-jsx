e5f970a16480f9651af7603730dcb3814e07d346
hamilton <hamilton.ulmer@gmail.com>
2019-07-10 19:08:24 -0700

    adds plugin def for fetch cell

--- docs/evaluate-jsx.js
+++ docs/evaluate-jsx.js
@@ -1,19 +1,21 @@
 const REACT = 'https://cdnjs.cloudflare.com/ajax/libs/react/16.4.2/umd/react.production.min.js'
-const REACT_DOM = 'https://hamilton.github.io/iodide-jsx/react-dom-build.js'
+const REACT_DOM = 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js'
 const BABEL_STANDALONE = 'https://unpkg.com/babel-standalone@6/babel.min.js'
 
 const loadResource = url => new Promise((resolve) => {
   const head = document.getElementsByTagName('head')[0];
   const theScript = document.createElement('script');
   theScript.src = url;
+  theScript.crossorigin = true;
   theScript.onload = () => {
     resolve();
   };
   head.appendChild(theScript);
 })
-
+console.log('THIS IS BZROKEN')
 Promise.all([loadResource(REACT), loadResource(REACT_DOM), loadResource(BABEL_STANDALONE)])
   .then(() => {
+    console.log(window)
     window.jsx = {}
     window.jsx.transpile = input => window.Babel.transform(input, {
       presets: [['es2015', { loose: true, modules: false }], 'react'],
